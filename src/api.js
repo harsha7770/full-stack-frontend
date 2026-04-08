373cddae7db86e4d2b09e9d9183a0eafae6bb548
unknown <undichandraharsha08@gmail.com>
2026-04-08 09:28:23 +0530

    Initial commit - frontend

---
+++ src/api.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'http://localhost:5000';
+
+export const api = {
+  login: `${BASE_URL}/login`,
+  register: `${BASE_URL}/register`,
+  users: `${BASE_URL}/users`,
+  mentors: `${BASE_URL}/mentors`,
+  mentees: `${BASE_URL}/mentees`,
+  match: `${BASE_URL}/match`,
+  sessions: `${BASE_URL}/sessions`,
+  progress: `${BASE_URL}/progress`,
+};
